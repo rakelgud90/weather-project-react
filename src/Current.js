c7e7b8eaa2b1e92f17be1126a21fc81201997952
rakelgud90 <72047432+rakelgud90@users.noreply.github.com>
2021-01-05 22:27:29 +0000

    modified the structure - added Current weather

--- src/Current.js
+++ src/Current.js
@@ -1,36 +1,25 @@
 import React from "react";
+import FormatDate from "./FormatDate";
 import "./styles.css";
 
-export default function Current() {
-  let weatherInfo = {
-    city: "London",
-    temperature: "3°C",
-    date: "Sun 6 December 2020 | Last Updated: 18:50",
-    description: "Sunny",
-    humidity: 70,
-    wind: "10 m/s",
-    min: "0°C",
-    max: "5°C"
-  };
-
-
-  
-
+export default function Current(props) {
+ 
   return (
-    <div className="card">
+    <div className="current-weather">
+     <div className="card">
       <div className="card-body">
         <div className="row">
           <div className="col-md-6">
             <h1 className="card-title" id="current-city">
-            {weatherInfo.city}
+            {props.data.city}
             </h1>
-            <span id="current-date"> {weatherInfo.date}</span>
+            <span id="current-date">  <FormatDate date={props.data.date} />  </span>
           </div>
           <div className="col-md-6 degrees">
             <div className="row">
               <div className="col-sm-6">
                 <span className="today-degrees">
-                  <span id="warmth-now"></span>
+                  <span id="warmth-now">{Math.round(props.data.temperature)}°C</span>
                 </span>
               </div>
               <span className="today-degrees">
@@ -47,32 +36,35 @@ export default function Current() {
                 </div>
               </span>
             </div>
+            
           </div>
+     
           <div className="col-md-6 more-info">
             <div className="row">
               <div className="col-md-6">
                 <span>Humidity: </span>
-                <span id="current-humidity">{weatherInfo.humidity}</span>%
+                <span id="current-humidity">{props.data.humidity}</span>%
               </div>
               <div className="col-md-6">
                 <span>Wind: </span>
-                <span id="current-wind">{weatherInfo.wind}</span>
+                <span id="current-wind">{props.data.wind}</span>
               </div>
               <div className="col-md-6">
                 <span>Min Temp: </span>
-                <span id="min-temp">{weatherInfo.min}</span>
+                <span id="min-temp">{Math.round(props.data.minTemp)}°C</span>
               </div>
               <div className="col-md-6">
                 <span>Max Temp: </span>
-                <span id="max-temp">{weatherInfo.max}</span>
+                <span id="max-temp">{Math.round(props.data.maxTemp)}°C</span>
               </div>
             </div>
           </div>
           <div className="col-md-6" id="description">
-            {weatherInfo.description}
+            {props.data.description}
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import FormatDate from "./FormatDate";
+import Current from "./Current"
 import Daily from "./Daily";
 import HourlyForecast from "./HourlyForecast";
 import "./styles.css";
@@ -47,67 +47,7 @@ export default function Search(props) {
 
   <div className="search-bar">
     <div className="col-sm-8 left-side">
-     <div className="current-weather">
-     <div className="card">
-      <div className="card-body">
-        <div className="row">
-          <div className="col-md-6">
-            <h1 className="card-title" id="current-city">
-            {weatherData.city}
-            </h1>
-            <span id="current-date">  <FormatDate date={weatherData.date} />  </span>
-          </div>
-          <div className="col-md-6 degrees">
-            <div className="row">
-              <div className="col-sm-6">
-                <span className="today-degrees">
-                  <span id="warmth-now">{Math.round(weatherData.temperature)}°C</span>
-                </span>
-              </div>
-              <span className="today-degrees">
-                <div className="col-sm-6">
-                  <span className="today-weather">
-                    <img
-                      src="http://openweathermap.org/img/wn/10d@2x.png"
-                      alt="icon"
-                      id="today-icon"
-                      width="120"
-                      height="120"
-                    />
-                  </span>
-                </div>
-              </span>
-            </div>
-            
-          </div>
-     
-          <div className="col-md-6 more-info">
-            <div className="row">
-              <div className="col-md-6">
-                <span>Humidity: </span>
-                <span id="current-humidity">{weatherData.humidity}</span>%
-              </div>
-              <div className="col-md-6">
-                <span>Wind: </span>
-                <span id="current-wind">{weatherData.wind}</span>
-              </div>
-              <div className="col-md-6">
-                <span>Min Temp: </span>
-                <span id="min-temp">{Math.round(weatherData.minTemp)}°C</span>
-              </div>
-              <div className="col-md-6">
-                <span>Max Temp: </span>
-                <span id="max-temp">{Math.round(weatherData.maxTemp)}°C</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6" id="description">
-            {weatherData.description}
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
+     <Current data={weatherData}/>
       <div className="row align-items-center justify-content-center">
         <div className="col-sm-3">
           <div
