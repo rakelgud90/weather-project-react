aa99844a68a5135657fb0d910d9cbbaed048a333
rakelgud90 <72047432+rakelgud90@users.noreply.github.com>
2021-01-07 22:21:46 +0000

    fixed my parent value issue

--- src/TemperatureUnit.js
+++ src/TemperatureUnit.js
@@ -16,39 +16,36 @@ export default function TemperatureUnit(props) {
 
     if (props.unit === "celsius") {
         return (
-            
-    <div className="current-weather">
-        <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-md-6">
+<div className="Temperature">
+<div className="current-weather">
+    <div className="card">
+        <div className="card-body">
+            <div className="row">
+              <div className="col-md-6">
                   <h1 className="card-title" id="current-city">
                   {props.data.city}
                   </h1>
                   <span id="current-date">  <FormatDate date={props.data.date} />  </span>
                 </div>
                 <div className="col-md-6 degrees">
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <span className="today-degrees">
-                        <span id="warmth-now">{Math.round(props.data.temperature)}°C</span>
-                      </span>
-                    </div>
-                    <span className="today-degrees">
-                      <div className="col-sm-6">
-                        <span className="today-weather">
-                          <img
-                           src={props.data.icon} alt={props.data.description}
-                            width="120"
-                            height="120"
-                          />
-                        </span>
-                      </div>
-                    </span>
+                    <div className="row">
+                        <div className="col-sm-6">
+                      		<span className="today-degrees">
+                       		    <span id="warmth-now">{Math.round(props.data.temperature)}°C</span>
+                     	     </span>
+                        </div>
+                    	<span className="today-degrees">
+
+
+                      	    <div className="col-sm-6">
+                        		<span className="today-weather">
+                          			<img src={props.data.icon} alt={props.data.description} width="120" height="120"
+                          	/>
+                       		 	</span>
+                     		</div>
+                   		 </span>
                   </div>
-                  
                 </div>
-           
                 <div className="col-md-6 more-info">
                   <div className="row">
                     <div className="col-md-6">
@@ -72,35 +69,33 @@ export default function TemperatureUnit(props) {
                 <div className="col-md-6" id="description">
                   {props.data.description}
                 </div>
-              </div>
-            </div>
-          </div>
-       
+
+             </div>
         </div>
+    </div>
+</div>
+ <div className="btn-group btn-group-toggle"
+ data-toggle="buttons"
+ id="toggle-button"
+>
+ <label className="btn btn-secondary active" id="celsius-converter">
+   <input type="radio" name="options" id="option1" checked onChange={showCelsius}/>
+   °C
+ </label>
+ <label className="btn btn-secondary" id="fahrenheit-converter">
+   <input type="radio" name="options" id="option2" /> °F
+ </label>
+</div>
+</div>
 
-    <div className="btn-group btn-group-toggle"
-    data-toggle="buttons"
-    id="toggle-button"
-  >
-    <label className="btn btn-secondary active" id="celsius-converter">
-      <input type="radio" name="options" id="option1" checked onChange={showCelsius}/>
-      °C
-    </label>
-    <label className="btn btn-secondary" id="fahrenheit-converter">
-      <input type="radio" name="options" id="option2" /> °F
-    </label>
-  </div>
-
-             );
-    } 
+        )}
     else 
-    {   
-        let fahrenheitTemperature = (props.data.temperature * 9) / 5 + 32;
+    {   let fahrenheitTemperature = (props.data.temperature * 9) / 5 + 32;
         let fahrenheitMaxTemp = (props.data.maxTemp * 9) / 5 + 32;
         let fahrenheitMinTemp = (props.data.minTemp * 9) / 5 + 32;
-
-        return ( 
         
+return (
+<div className="Temperature">
     <div className="current-weather">
         <div className="card">
             <div className="card-body">
@@ -111,6 +106,8 @@ export default function TemperatureUnit(props) {
                   </h1>
                   <span id="current-date">  <FormatDate date={props.data.date} />  </span>
                 </div>
+
+
                 <div className="col-md-6 degrees">
                   <div className="row">
                     <div className="col-sm-6">
@@ -175,7 +172,7 @@ export default function TemperatureUnit(props) {
       <input type="radio" name="options" id="option2" /> °F
     </label>
   </div>
-
-);
+</div>
+ )
 }
 }
